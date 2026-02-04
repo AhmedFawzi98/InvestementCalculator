b6c3f226497689b7b23138b918e5ecd11a07636c
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-02-04 16:39:28 +0200

    Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [],
+    imports: [HeaderComponent],
     standalone: true,
 })
 export class AppComponent {}
